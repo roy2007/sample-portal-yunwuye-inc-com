8d5c3197a0e9a325d0a812956d8b7e11925c73e5
Royrui2007 <13439298452@163.com>
2021-09-12 14:29:41 +0800

    增加分布式同步与异步修改数据库对应事务回滚测试，目前测试如果通过CompletableFuture做多线程批理修改，无法保证完整事务

--- sample-portal-yunwuye-inc-com-controller/src/main/java/com/yunwuye/sample/controller/student/StudentController.java
+++ sample-portal-yunwuye-inc-com-controller/src/main/java/com/yunwuye/sample/controller/student/StudentController.java
@@ -1,5 +1,8 @@
 package com.yunwuye.sample.controller.student;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -7,7 +10,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.yunwuye.sample.common.base.result.Result;
 import com.yunwuye.sample.controller.BaseController;
@@ -22,21 +24,69 @@ import com.yunwuye.sample.service.StudentService;
  * @date 2020年6月27日-下午10:42:32
  */
 @RestController
-@RequestMapping("/student")
+@RequestMapping ("/student")
 @EnableAutoConfiguration
-public class StudentController extends BaseController {
-  Logger logger = LoggerFactory.getLogger(StudentController.class);
+public class StudentController extends BaseController{
 
-  @Reference(group = "studentService", version = "1.0", check = false)
-  private StudentService studentService;
+    Logger                 logger = LoggerFactory.getLogger (StudentController.class);
+    @Reference (group = "studentService", version = "1.0", check = false)
+    private StudentService studentService;
 
-  @GetMapping("/find")
-  public Result<StudentVO> findStudentById(Long id) {
-    Result<StudentVO> result = new Result<>();
-    StudentDTO dto = studentService.findById(id);
-    StudentVO target = new StudentVO();
-    BeanUtils.copyProperties(dto, target);
-    result.setData(target);
-    return result;
-  }
+    @GetMapping ("/find")
+    public Result<StudentVO> findStudentById (Long id) {
+        Result<StudentVO> result = new Result<> ();
+        StudentDTO dto = studentService.findById (id);
+        StudentVO target = new StudentVO ();
+        BeanUtils.copyProperties (dto, target);
+        result.setData (target);
+        return result;
+    }
+
+    /**
+     * 
+     * @param isAsync 是否异步处理
+     * @return
+     */
+    @GetMapping ("/batchUpdateById")
+    public Result<Integer> batchUpdateStudentById (String isAsync) {
+        if (StringUtils.isBlank (isAsync)) {
+            isAsync = "0";
+        }
+        Result<Integer> result = new Result<> ();
+        List<StudentDTO> dtos = new ArrayList<StudentDTO> ();
+        StudentDTO dto1 = new StudentDTO ();
+        dto1.setId (1L);
+        dto1.setName ("tom");
+        dto1.setAge (10);// sucess
+        dtos.add (dto1);
+        StudentDTO dto2 = new StudentDTO ();
+        dto2.setId (2L);
+        dto2.setName ("johe");
+        dto2.setAge (10);
+        dtos.add (dto2);
+        StudentDTO dto3 = new StudentDTO ();
+        dto3.setId (3L);
+        dto3.setName ("roy");// sucess
+        dto3.setAge (10);
+        dtos.add (dto3);
+        StudentDTO dto4 = new StudentDTO ();
+        dto4.setId (4L);
+        dto4.setName ("mary");
+        dto4.setAge (10);
+        dtos.add (dto4);
+        StudentDTO dto5 = new StudentDTO ();
+        dto5.setId (5L);
+        dto5.setName ("saly");// sucess
+        dto5.setAge (10);
+        dtos.add (dto5);
+        Integer count = 0;
+        /**只要不等于0，表示是异步，如果等于0表示同步*/
+        if (!"0".equals (isAsync)) {
+            count = studentService.batchAsyncUpdateStudentById (dtos);
+        } else {
+            count = studentService.batchSyncUpdateStudentById (dtos);
+        }
+        result.setData (count);
+        return result;
+    }
 }
